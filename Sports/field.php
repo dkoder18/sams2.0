8afffe7192dbd714a6f9baa5389c642731a4c3ac
SamC12345 <student979@live.com>
2019-03-01 11:21:37 -0500

    changed som styling and added navbar

--- Sports/field.php
+++ Sports/field.php
@@ -4,7 +4,7 @@ $file = fopen("../data.csv", 'r') or die("Unable to open file!"); // Opens the f
 
 
 $out=<<<EOD
-<table class="table">
+<table class="table table-bordered">
   <thead>
     <tr>
       <th scope="col"><p class="text-center">First Name</p></th>
@@ -71,28 +71,29 @@ while (($data = fgetcsv($file)) !== FALSE) {
     <td><p class="text-center">$age</p></td>
     <td><p class="text-center">$gender</p></td>
     <td><p class="text-center">$city</p></td>
-    <td><p class="text-center"><a class="btn btn-info" href="https://web.njit.edu/~jsp74/sams2.0/masterpage/athleteRecords.php?id=$id" role="button">Info</a></p></td>
+    <td><p class="text-center"><a id="infoBtn" class="btn btn-info" href="https://web.njit.edu/~jsp74/sams2.0/masterpage/athleteRecords.php?id=$id" role="button">Info</a></p></td>
   </tr>
 EOD;
 
-    // echo $row;
     $out.=$row;
-    echo "<br/>";
 }
 
 $out.=<<<EOD
     </tbody>
 </table>
 EOD;
+
+// echo $out;
 ?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>USA</title>
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
@@ -100,6 +101,7 @@ EOD;
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Do+Hyeon" rel="stylesheet">
     
     <style>
         
@@ -140,35 +142,165 @@ EOD;
             padding: 20px 0;
             font-size: 30px;
         }
-        .col {
-            border: 5px;
-            border-color: black
+        
+        /******************************* Page Title Start Here *******************************/
+        
+        body{
+            background-image: linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%);
+        }
+        
+        .jumbotron{
+            background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
+            box-shadow: inset 0px 0px 5px 1px rgba(0,0,0,0.3);
+            margin-bottom: 0;
+        }
+        
+        .heading{
+            text-align: center;
+            font-weight: bold;
+            font-family: 'Do Hyeon', sans-serif;
             
         }
-
-        .nopadding {
-        padding: 1px !important;
-        margin: 0 !important;
+        
+        .navbar{
+            border-radius: 0 !important;
+        }
+        
+        /******************************* DropDown Menu Start Here *******************************/
+        
+        .btn{
+            width: 280px;
+            height: 60px;
+            font-size: 16px;
+            border-radius: 5px;
+            font-weight: bold;
+            margin-top: 10px;
+        }
+        
+        .row .btn a{
+            color: white;
+            text-decoration: none;
         }
 
+        
         .list-group-item{
             padding: 0px;
+            background: inherit;
+            /* border-radius: 10px; */
+        }
+        .list-group{
+            /* border-radius: inherit; */
+            margin: 0px;
+        }
+        #infoBtn{
+            height: 2em;
+            width: 4em;
         }
 
 
-         
-
-        
-        
     </style>
     
     </head>
-    <body>  
+    
+    <body>
+        
+        <div class = "jumbotron">
+            <div class = "heading">
+                <h2 id = "title1">Sports Available Management Software</h2>
+                <h3 id = "title2">Adaptive Sports USA 2017 Junior Nationals</h3>
+            </div>
+        </div>
+        
+        <nav class="navbar navbar-inverse">
+            <div class="navbar-header">
+                <a class="navbar-brand"  href="SAMShome.php"  style="color: #B3DA2E;">Dashboard</a>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>                        
+                </button>
+            </div>
+
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+
+                    <!-- ALll nav-pages under this nav-bar is moved to masterpage folder -->
+                    <!-- Pages commeted out will be taken care og eventually as the project moves forward -->
+
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Master Page <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <!-- <li><a id = "master_file" href="masterpage/masterfile.html">Master Help File</a></li>
+                            <li><a id = "how_to" href="masterpage/howto.html">How to do it</a></li> -->
+                            <li><a id = "register_form" href = "masterpage/registrationform.php">Registration Form</a></li>
+                            <li><a id = "find_register" href = "masterpage/findregistration.html">Find All Registration</a></li>
+                            <li><a id = "setup_pref" href = "masterpage/setupmaster.php">Setup Master Preferences</a></li>
+                            <!-- <li><a id = "athlete_perform_record" href = "masterpage/athleteperform.html">Athlete Performance Records</a></li>
+                            <li><a id = "qualifying_std" href = "masterpage/qualifyingstand.html">Qualifying Standards</a></li>
+                            <li><a id = "set_relay" href = "masterpage/setrelay.html">Set Relay Points</a></li>
+                            <li><a id = "see_credit" href = "masterpage/seecreditscreen.html">See Credit Screen</a></li>
+                            <li><a id = "photo_database" href = "masterpage/photodatabase.html">Photo Database</a></li>
+                            <li><a id = "classification" href = "masterpage/classificationfile.html">Classification file</a></li>
+                            <li><a id = "Implements" href = "masterpage/implementsusedfile.html">Implements used file</a></li>
+                            <li><a id = "pentathion" href = "masterpage/performancepointssetup.html">Performance Points Setup</a></li> -->
+                        </ul>
+                    </li>
+
+                    <!-- ALll nav-pages under this nav-bar is moved to resultpage folder -->
+
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Result International Format <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a id = "extrackfield" href="resultpage/exporttrack.html">Export Track &amp; Field</a></li>
+                            <li><a id = "exswimming" href="resultpage/exportswimming.html">Export Swimming</a></li>
+                        </ul>
+                    </li>
+
+                    <!-- ALll nav-pages under this nav-bar is moved to preferences folder -->
+
+                    <!-- <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Preferences <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a id = "backup" href="preferences/backuppreferences.html">Backup Preferences</a></li>
+                            <li><a id = "restore" href="preferences/restorepreferences.html">Restore Preferences</a></li>
+                        </ul>
+                    </li> -->
+
+                    <!-- All the Sports -->
+
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Sports <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a id = "archery" href="#">Archery</a></li>
+                            <li><a id = "fencing" href="#">Fencing</a></li>
+                            <li><a id = "field" href="#">Field</a></li>
+                            <li><a id = "swimming" href="#">Swimming</a></li>
+                            <li><a id = "tabletennis" href="#">Table Tennis</a></li>
+                            <li><a id = "teamsports" href="#">Team Sports</a></li>
+                            <li><a id = "track" href="#">Track</a></li>
+                            <li><a id = "other" href="#">Other Sports</a></li>
+                        </ul>
+                    </li>
+                    
+                </ul>
+
+                <ul class="nav navbar-nav navbar-right dropdown">
+                    <li>
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Profile <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="login/logout.php">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        
+        <div style="padding: 30px;">
         <?php
-
-        echo $out;
-
+            echo $out;
         ?>
+</div>
+
+       
     </body>
 </html>
-<script>
